63b6d8c1f69b440e96d33fde356584fa2e2a3950
Izumi <izuminft@gmail.com>
2022-02-28 03:36:19 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -249,7 +249,7 @@ function App() {
             >
               <StyledButton
                 onClick={(e) => {
-                  window.open("/config/roadmap.pdf", "_blank");
+                  window.open("/config/roadma.pdf", "_blank");
                 }}
                 style={{
                   margin: "5px",
